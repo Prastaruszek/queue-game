c83aa942267875e051c7e70fc54a4a2b7d59e521
Michał laptop <michal@localhost.localdomain>
2013-04-30 08:16:17 +0200

    maven file structure added

---
+++ src/main/java/queue_game/App.java
@@ -0,0 +1,13 @@
+package queue_game;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
